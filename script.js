527a832ee96972cb2444bf2da1d70d2a11441867
ChefKayl <kylejessie.lumeran@hcdc.edu.ph>
2025-11-29 13:57:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage() {
-    alert("Thank You for purchase!");
+    let drink = document.getElementById("drinks").value;
+    let drinkQty = document.getElementById("drinkQty").value;
+
+    let food = document.getElementById("foods").value;
+    let foodQty = document.getElementById("foodQty").value;
+
+    let total = (drink * drinkQty) + (food * foodQty);
+
+    document.getElementById("totalText").innerText = "Total: ₱" + total;
+    alert("Thank you for purchase!\nYour total is ₱" + total);
 }
